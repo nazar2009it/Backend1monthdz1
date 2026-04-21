917074371ff0d160d34447297d549a933522aaed
Назар <ratbekovnazar@gmail.com>
2026-04-21 19:19:23 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+monday = int(input("Понедельник: "))
+tuesday = int(input("Вторник: "))
+wednesday = int(input("Среда: "))
+thursday = int(input("Четверг: "))
+friday = int(input("Пятница: "))
+saturday = int(input("Суббота: "))
+sunday = int(input("Воскресенье: "))
+
+total = monday + tuesday + wednesday + thursday + friday + saturday + sunday
+average = total // 7
+
+print("Общая сумма:", total)
+print("Средний расход:", average)
+
+
+
+
+
+
+
