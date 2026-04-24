50e860345662f2e7a81e525c7a2cb861093df45b
Назар <ratbekovnazar@gmail.com>
2026-04-24 19:09:05 +0600

    dz2

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-monday = int(input("Понедельник: "))
-tuesday = int(input("Вторник: "))
-wednesday = int(input("Среда: "))
-thursday = int(input("Четверг: "))
-friday = int(input("Пятница: "))
-saturday = int(input("Суббота: "))
-sunday = int(input("Воскресенье: "))
-
-total = monday + tuesday + wednesday + thursday + friday + saturday + sunday
-average = total // 7
-
-print("Общая сумма:", total)
-print("Средний расход:", average)
-
+day = int(input("Введите день рождения: "))
+month = int(input("Введите месяц рождения: "))
+if (day < 1 or day > 31) or (month < 1 or month > 12):
+    print("Неверная дата")
+else:
+    if (day >= 21 and month == 3) or (day <= 20 and month == 4):
+        print("Овен")
+    elif (day >= 21 and month == 4) or (day <= 21 and month == 5):
+        print("Телец")
+    elif (day >= 22 and month == 5) or (day <= 21 and month == 6):
+        print("Близнецы")
+    elif (day >= 22 and month == 6) or (day <= 22 and month == 7):
+        print("Рак")
+    elif (day >= 23 and month == 7) or (day <= 21 and month == 8):
+        print("Лев")
+    elif (day >= 22 and month == 8) or (day <= 23 and month == 9):
+        print("Дева")
+    elif (day >= 24 and month == 9) or (day <= 23 and month == 10):
+        print("Весы")
+    elif (day >= 24 and month == 10) or (day <= 22 and month == 11):
+        print("Скорпион")
+    elif (day >= 23 and month == 11) or (day <= 22 and month == 12):
+        print("Стрелец")
+    elif (day >= 23 and month == 12) or (day <= 20 and month == 1):
+        print("Козерог")
+    elif (day >= 21 and month == 1) or (day <= 19 and month == 2):
+        print("Водолей")
+    elif (day >= 20 and month == 2) or (day <= 20 and month == 3):
+        print("Рыбы")
 
 
 
